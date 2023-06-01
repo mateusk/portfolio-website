3bc0e73c355db7f5a5695a30c3255783e0b2d814
Mateus Knelsen <sapientia_@hotmail.com>
2023-06-01 23:38:00 +0200

    Add configs for @nuxt/image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,29 @@
 export default defineNuxtConfig({
   srcDir: 'src/',
   css: ['~~/src/assets/css/main.css'],
+  modules: ['@nuxt/content', '@nuxt/image-edge', '@nuxtjs/device'],
   nitro: {
     preset: 'firebase',
   },
-  modules: ['@nuxt/content', '@nuxt/image-edge', '@nuxtjs/device'],
+  image: {
+    dir: 'assets/images',
+    screens: {
+      xs: 320,
+      sm: 640,
+      md: 768,
+      lg: 1024,
+      xl: 1280,
+      xxl: 1536,
+      '2xl': 1536,
+    },
+    presets: {
+      project: {
+        modifiers: {
+          fit: 'cover',
+          format: 'webp',
+          quality: 80,
+        },
+      },
+    },
+  },
 })
